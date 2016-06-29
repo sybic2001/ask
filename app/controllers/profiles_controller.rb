ac83bf0d6a27f802f3942308718209e8d70deadb
Arthur Vinson <arthur.vinson@polytechnique.org>
2016-06-29 23:42:06 +0200

    front profile in progress

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,2 +1,9 @@
 class ProfilesController < ApplicationController
+  def show
+    if current_user.id == params[:id].to_i
+      render :show_private
+    else
+      render :show_public
+    end
+  end
 end
