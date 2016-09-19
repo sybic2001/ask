a21877fa7c7816ee167dc6b6f395d590fe37397e
Arthur Vinson <arthur.vinson@polytechnique.org>
2016-09-19 19:18:07 +0200

    nouvelle arborescence des competences

--- app/models/competency.rb
+++ app/models/competency.rb
@@ -4,11 +4,15 @@ class Competency < ActiveRecord::Base
   has_many :child_competencies, class_name: "Competency", foreign_key: "parent_competency_id", dependent: :destroy
   has_many :user_competencies, dependent: :destroy
 
-  validates :name, :description, presence: true
+  validates :name, presence: true
   validates :name, uniqueness: true
 
   algoliasearch do
     attribute :name
+  end
+
+  def self.parent_competencies
+    Competency.all.where(parent_competency_id: nil)
   end
 
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,47 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+comp = Competency.create(name: 'Culture Startup')
+Competency.create(name: 'Art du Pitch', parent_competency: comp)
+Competency.create(name: 'Lean Startup', parent_competency: comp)
+Competency.create(name: 'Growthhacking', parent_competency: comp)
+Competency.create(name: 'Levée de fonds', parent_competency: comp)
+
+comp = Competency.create(name: 'Marketing et Ventes')
+Competency.create(name: 'Etude de marché', parent_competency: comp)
+Competency.create(name: "Construction de l'offre", parent_competency: comp)
+Competency.create(name: 'Stratégie commerciale', parent_competency: comp)
+
+comp = Competency.create(name: 'Finance')
+Competency.create(name: 'Comptabilité et gestion', parent_competency: comp)
+Competency.create(name: 'Business plan', parent_competency: comp)
+
+comp = Competency.create(name: 'Juridique')
+Competency.create(name: 'Social - Droit du travail', parent_competency: comp)
+Competency.create(name: 'Propriété intellectuelle - Marque - Brevet', parent_competency: comp)
+Competency.create(name: 'Forme juridique - Statuts - Pacte', parent_competency: comp)
+
+comp = Competency.create(name: 'Développement')
+Competency.create(name: 'Ruby', parent_competency: comp)
+Competency.create(name: 'JS', parent_competency: comp)
+Competency.create(name: 'HTML - CSS', parent_competency: comp)
+Competency.create(name: 'React', parent_competency: comp)
+Competency.create(name: 'Java', parent_competency: comp)
+Competency.create(name: 'React', parent_competency: comp)
+Competency.create(name: 'Python', parent_competency: comp)
+Competency.create(name: 'PHP', parent_competency: comp)
+
+comp = Competency.create(name: "Systèmes d'informations")
+Competency.create(name: 'Big data', parent_competency: comp)
+Competency.create(name: 'Réseaux et architecture', parent_competency: comp)
+Competency.create(name: 'SEO et SEA', parent_competency: comp)
+
+comp = Competency.create(name: 'Communication')
+Competency.create(name: 'Plan de communication', parent_competency: comp)
+Competency.create(name: 'Outils de communication - Création', parent_competency: comp)
+
+comp = Competency.create(name: 'Ressources Humaines')
+Competency.create(name: 'Recrutement', parent_competency: comp)
+
+Competency.create(name: 'Divers')
