e9677363728c12dd2e886e1d3bb1e748620f4983
Arthur Vinson <arthur.vinson@polytechnique.org>
2016-09-22 18:49:03 +0200

    test ajax devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   mount Attachinary::Engine => "/attachinary"
-  devise_for :users # :controllers => {registrations: 'registrations'}
+  devise_for :users, :controllers => {sessions: 'sessions'}
   root to: 'pages#home'
 
   get "users/:id/profile", to: "profiles#show", as: "profile"
