0a723ddfb2c06f09e041e6e62fadb71830a6faa6
Arthur Vinson <arthur.vinson@polytechnique.org>
2016-12-13 14:22:01 +0100

    modif en cours

--- app/models/membership.rb
+++ app/models/membership.rb
@@ -2,7 +2,7 @@ class Membership < ActiveRecord::Base
   belongs_to :community
   belongs_to :user
 
-  validates :community, :user, :status, presence: true
+  validates :community, :user, :status, :promotion, presence: true
   validates :community, uniqueness: { scope: :user }
   validates :status, inclusion: { in: ["pending_approval","member","refused","left","excluded"]}
 end
