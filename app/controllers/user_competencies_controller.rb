bd1d0c101825cce72a50da847c68831f3f20f8e8
Arthur Vinson <arthur.vinson@polytechnique.org>
2017-01-06 13:06:04 +0100

    search filter fonctionnel

--- app/controllers/user_competencies_controller.rb
+++ app/controllers/user_competencies_controller.rb
@@ -9,33 +9,6 @@ class UserCompetenciesController < ApplicationController
     @user_competency = UserCompetency.find(params[:id])
   end
 
-  def search
-    # @cities = params[:filters][:cities].reject(&:empty?)
-    @competency_id = params[:filters][:competency_id]
-    @communities = params[:filters][:communities].reject(&:empty?)
-    @cities = params[:filters][:cities].reject(&:empty?)
-    @levelmin = params[:filters][:level].to_i
-    @peer_competencies = UserCompetency.where(user: current_user.peers).uniq
-    @peer_competencies = current_user.favorite_user_competencies if params[:filters][:favorite] == "true"
-    if params[:filters][:promo] == "true"
-      @promo_peers = []
-      current_user.memberships.where(status: "member").where.not(promotion: nil).each do |m|
-        @promo_peers << current_user.promo_peers(m.community_id)
-      end
-      @peer_competencies = @peer_competencies.where(user: @promo_peers)
-    end
-    if !@competency_id.empty?
-      @peer_competencies = @peer_competencies.where(competency_id: @competency_id)
-      params[:user_competency][:competency_ids].reject(&:empty?).each do |badge_id|
-        @peer_competencies = @peer_competencies.joins(:badges).where("badges.competency_id = ?", badge_id)
-      end
-    end
-    @peer_competencies = @peer_competencies.joins(user: :memberships).where(memberships: { community_id: @communities} ) unless @communities.empty?
-    @peer_competencies = @peer_competencies.where('level >= ?', @levelmin) unless @levelmin.nil?
-    @peer_competencies = @peer_competencies.includes(user: :profile).where(profiles: { city: @cities }) unless @cities.empty?
-    @peer_competencies = @peer_competencies.order(:competency_id, level: :desc)
-  end
-
   def new
     @user_competency = UserCompetency.new()
     @experience = Experience.new()
@@ -76,6 +49,35 @@ class UserCompetenciesController < ApplicationController
         format.js  # <-- will render `app/views/reviews/update.js.erb`
       end
     end
+  end
+
+  def search
+    # @cities = params[:filters][:cities].reject(&:empty?)
+    @competency_id = params[:filters][:competency_id]
+    @communities = params[:filters][:communities].reject(&:empty?)
+    @cities = params[:filters][:cities].reject(&:empty?)
+    @levelmin = params[:filters][:level].to_i
+    @peer_competencies = UserCompetency.where(user: current_user.peers).uniq
+    @peer_competencies = current_user.favorite_user_competencies if params[:filters][:favorite] == "true"
+    if params[:filters][:promo] == "true"
+      @promo_peers = []
+      current_user.memberships.where(status: "member").where.not(promotion: nil).each do |m|
+        @promo_peers << current_user.promo_peers(m.community_id)
+      end
+      @peer_competencies = @peer_competencies.where(user: @promo_peers)
+    end
+    if !@competency_id.empty?
+      @peer_competencies = @peer_competencies.where(competency_id: @competency_id)
+      sql_seq = []
+      params[:user_competency][:competency_ids].reject(&:empty?).each do |badge_id|
+        sql_seq << "EXISTS (SELECT 1 FROM badges WHERE badges.competency_id = #{badge_id.to_i} AND badges.user_competency_id = user_competencies.id)"
+      end
+      @peer_competencies = @peer_competencies.where(sql_seq.join(" AND ")) unless sql_seq.empty?
+    end
+    @peer_competencies = @peer_competencies.joins(user: :memberships).where(memberships: { community_id: @communities} ) unless @communities.empty?
+    @peer_competencies = @peer_competencies.where('level >= ?', @levelmin) unless @levelmin.nil?
+    @peer_competencies = @peer_competencies.includes(user: :profile).where(profiles: { city: @cities }) unless @cities.empty?
+    @peer_competencies = @peer_competencies.order(:competency_id, level: :desc)
   end
 
   private
